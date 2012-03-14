676270cbbf56443590fb8f2b00aa4186f7f9a51c
Piotr Murach <pmurach@gmail.com>
2012-03-14 22:38:51 +0000

    Add test harness.

---
+++ spec/github_cli/cli_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe GithubCli::CLI do
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'github_cli'
+
+RSpec.configure do |config|
+  config.order = :rand
+end
