33005b3757e3f3674128545f1ef361ab7eab6ad5
Piotr Murach <pmurach@gmail.com>
2012-03-25 20:39:53 +0100

    Add initial repository command.

--- lib/github_cli/repositories.rb
+++ lib/github_cli/repositories.rb
@@ -1,4 +1,32 @@
-module GithubCli
-  class Repositories < Commands
-  end
-end
+# encoding: utf-8
+
+require 'github_cli/command'
+
+module GithubCLI
+  class Repositories < Command
+
+    namespace :repo
+
+    option :orgs, :type => :boolean,
+           :desc => 'List repositories for <organization>'
+    desc 'list', 'Listing all repositories'
+    def list
+      say 'listing'
+    end
+
+    desc 'get', 'Get a repository'
+    def get
+    end
+
+    desc 'create', 'Create <repo> <user>'
+    def create
+      say 'creating...'
+    end
+
+    desc 'remove', 'Remove <repo> <user>'
+    def remove
+      say 'removing'
+    end
+
+  end # Repositories
+end # GithubCLI
