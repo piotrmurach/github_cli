126634ea75709c41c7c450c434432435c35b320c
Piotr Murach <pmurach@gmail.com>
2012-03-25 21:02:24 +0100

    Add required lib files.

--- lib/github_cli.rb
+++ lib/github_cli.rb
@@ -1,11 +1,20 @@
 # encoding: utf-8
 
 require 'thor'
+require 'thor/group'
 require 'github_api'
-require "github_cli/version"
-require "github_cli/cli"
-require "github_cli/commands"
-require "github_cli/repositories"
 
-module GithubCli
+module GithubCLI
+  require "github_cli/cli"
+
+  require "github_cli/version"
+  require "github_cli/api"
+  require "github_cli/config"
+  require "github_cli/repository"
+  require "github_cli/repositories"
+  require "github_cli/issues"
+  require "github_cli/command"
+
+  autoload :CLI, 'github_cli/cli'
+  autoload :Repositories, 'github_cli/repositories'
 end
