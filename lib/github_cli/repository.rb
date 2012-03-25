54409e7bd034191a577820d353d986f65e4556ca
Piotr Murach <pmurach@gmail.com>
2012-03-25 21:02:50 +0100

    Add repo stub.

---
+++ lib/github_cli/repository.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+module GithubCLI
+  class Repository
+
+    def initialize
+    end
+
+    def self.all
+    end
+
+    def self.get(id)
+
+    end
+
+    def self.create()
+    end
+
+  end # Repository
+end # GithubCLI
