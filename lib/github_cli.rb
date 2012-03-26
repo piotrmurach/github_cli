1db9ce68939857a5239be3ad6f416c887abca593
Piotr Murach <pmurach@gmail.com>
2012-03-26 22:53:10 +0100

    Require ui.

--- lib/github_cli.rb
+++ lib/github_cli.rb
@@ -3,11 +3,9 @@
 require 'thor'
 require 'thor/group'
 require 'github_api'
+require "github_cli/version"
 
 module GithubCLI
-  require "github_cli/cli"
-
-  require "github_cli/version"
   require "github_cli/api"
   require "github_cli/config"
   require "github_cli/repository"
@@ -17,4 +15,13 @@ module GithubCLI
 
   autoload :CLI, 'github_cli/cli'
   autoload :Repositories, 'github_cli/repositories'
-end
+  autoload :UI, 'github_cli/ui'
+
+  class << self
+    attr_writer :ui
+
+    def ui
+      @ui ||= UI.new
+    end
+  end
+end # GithubCLI
