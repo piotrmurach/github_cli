5cc6c7ffc69f137e31c7f2fcd482a0c2198416bc
Piotr Murach <pmurach@gmail.com>
2012-03-26 22:52:49 +0100

    Initialize shell and describe options.

--- lib/github_cli/cli.rb
+++ lib/github_cli/cli.rb
@@ -6,6 +6,19 @@ require "github_cli/issues"
 
 module GithubCLI
   class CLI < ::Thor
+    include Thor::Actions
+
+    def initialize(*args)
+      super
+      say <<-TEXT
+
+Github CLI client
+
+      TEXT
+      the_shell = (options["no-color"] ? Thor::Shell::Basic.new : shell)
+      GithubCLI.ui = UI.new(the_shell)
+      GithubCLi.ui.debug! if options["verbose"]
+    end
 
     map "repo" => :repository,
         "is" => :issue,
@@ -15,17 +28,13 @@ module GithubCLI
                  :desc => "Configuration file.",
                  :default => "~/.githubrc"
     class_option :oauth, :type => :string, :aliases => '-a',
-                 :desc => 'Authentication token.'
-    class_option :verbose, :type => :boolean
+                 :desc => 'Authentication token.',
+                 :banner => 'Set authentication token'
+    class_option "no-color", :type => :boolean,
+                 :banner => "Disable colorization in output."
+    class_option :verbose, :type => :boolean,
+                 :banner => "Enable verbose output mode."
 
-    def initialize(*args)
-      super
-      say <<-TEXT
-
-Github CLI client
-
-      TEXT
-    end
 
     desc 'init <config-name>', 'Initialize configuration file'
     def init(config_name=nil)
