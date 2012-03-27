ee05fa457d386f62e3ed502e0e9c1fb981fcdaf4
Piotr Murach <pmurach@gmail.com>
2012-03-27 23:11:48 +0100

    Change required files.

--- lib/github_cli.rb
+++ lib/github_cli.rb
@@ -7,21 +7,25 @@ require "github_cli/version"
 
 module GithubCLI
   require "github_cli/api"
-  require "github_cli/config"
   require "github_cli/repository"
   require "github_cli/repositories"
   require "github_cli/issues"
   require "github_cli/command"
 
-  autoload :CLI, 'github_cli/cli'
+  autoload :Config,       'github_cli/config'
+  autoload :CLI,          'github_cli/cli'
   autoload :Repositories, 'github_cli/repositories'
-  autoload :UI, 'github_cli/ui'
+  autoload :UI,           'github_cli/ui'
 
   class << self
     attr_writer :ui
 
     def ui
       @ui ||= UI.new
+    end
+
+    def config
+      @config ||= GithubCLI::Config.new
     end
   end
 end # GithubCLI
