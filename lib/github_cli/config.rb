49673e5116bd4061a5a362fef33bdca4b342b7f3
Piotr Murach <pmurach@gmail.com>
2012-03-27 23:10:35 +0100

    Add configuration manager stub.

--- lib/github_cli/config.rb
+++ lib/github_cli/config.rb
@@ -2,13 +2,29 @@
 
 module GithubCLI
   class Config
+    COMMAND_KEY = 'commands'
 
-    def initialize(options={})
+    def initialize(config_filename, options={})
+      @filename = config_filename
       @options = options
     end
 
-    def self.config
-      ::GithubCLI.const_set(:CONFIG, self.new)
+    # TODO: All commands go in there as well
+    def save(config)
+      File.open(@filename, 'w', 0600) do |file|
+        YAML.dump(config, file)
+      end
     end
+
+    def load
+      File.open(@filename, 'r') do |file|
+        YAML.load(file)
+      end
+    end
+
+    def global_config_file
+      "#{ENV['HOME']}/#{@filename}"
+    end
+
   end
 end # GithubCLI
