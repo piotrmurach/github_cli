4ebb5a09a4f87b481498df31375baeefcbea086a
Piotr Murach <pmurach@gmail.com>
2012-03-31 21:56:47 +0100

    Add stub for labels access.

---
+++ lib/github_cli/labels.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+require 'github_cli/command'
+
+module GithubCLI
+  class Labels < Command
+
+    namespace :label
+
+    desc 'list', 'Listing all labels for this repository.'
+    def list
+    end
+
+    desc 'get <user>, <repo>', 'Get a single label.'
+    method_option :name, :type => :string, :required => true
+    def get(user, repo)
+    end
+
+    desc 'create <user>, <repo>', 'Create a label.'
+    method_option :params, :type => :hash
+    def create(user, repo)
+    end
+
+    method_option :name, :type => :string, :required => true
+    desc 'update <user>, <repo>', 'Create a label.'
+    method_option :params, :type => :hash
+    def update(user, repo)
+    end
+
+    method_option :name, :type => :string, :required => true
+    desc 'delete <user>, <repo>', 'Delete a label.'
+    method_option :params, :type => :hash
+    def delete(user, repo)
+    end
+
+  end # Labels
+end # GithubCLI
