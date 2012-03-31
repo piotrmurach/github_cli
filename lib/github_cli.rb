4ec912e57e31b6afe13b5c0915e529c6b2770001
Piotr Murach <pmurach@gmail.com>
2012-03-31 21:59:01 +0100

    Change libraries loading order.

--- lib/github_cli.rb
+++ lib/github_cli.rb
@@ -4,19 +4,21 @@ require 'yaml'
 require 'thor'
 require 'thor/group'
 require 'github_api'
-require "github_cli/version"
+require 'github_cli/version'
 
 module GithubCLI
+  autoload :Config,       'github_cli/config'
+  autoload :CLI,          'github_cli/cli'
+  autoload :Terminal,     'github_cli/terminal'
+  autoload :Repositories, 'github_cli/repositories'
+  autoload :UI,           'github_cli/ui'
+
   require "github_cli/api"
   require "github_cli/repository"
   require "github_cli/repositories"
   require "github_cli/issues"
+  require "github_cli/labels"
   require "github_cli/command"
-
-  autoload :Config,       'github_cli/config'
-  autoload :CLI,          'github_cli/cli'
-  autoload :Repositories, 'github_cli/repositories'
-  autoload :UI,           'github_cli/ui'
 
   class << self
     attr_writer :ui, :config
@@ -27,6 +29,10 @@ module GithubCLI
 
     def config
       @config ||= GithubCLI::Config.new
+    end
+
+    def commands
+      @commands ||= GithubCLI::Command.all
     end
   end
 end # GithubCLI
