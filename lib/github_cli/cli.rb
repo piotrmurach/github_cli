9cd351b1628dd533bc4f31f4fba98af8b4df7e0b
Piotr Murach <pmurach@gmail.com>
2012-03-31 16:02:09 +0100

    Add initial setup for config init.

--- lib/github_cli/cli.rb
+++ lib/github_cli/cli.rb
@@ -26,7 +26,7 @@ Github CLI client
 
     class_option :config, :type => :string,
                  :desc => "Configuration file.",
-                 :default => "~/.githubrc"
+                 :default => ".githubrc"
     class_option :oauth, :type => :string, :aliases => '-a',
                  :desc => 'Authentication token.',
                  :banner => 'Set authentication token'
@@ -38,17 +38,29 @@ Github CLI client
 
     desc 'init <config-name>', 'Generates a configuration file in your home directory'
     long_desc <<-DESC
-      Init generates
+      Initializes a configuration file where you can set default options for
+      interacting with GitHub API. Both global and per-command options can be
+      specified. These defaults override the bult-in defaults and allow you to
+      omit commonly used command line options.
     DESC
-    method_option :force, :type => :boolean, :aliases => "-f",
+    method_option :force, :type => :boolean, :default => false, :aliases => "-f",
                   :banner => "Overwrite configuration file. "
-    def init(config_name=nil)
-      if !options[:force] || File.exists?(options[:config])
-        GithubCLI.ui.error "Not overwritting existing config file #{options[:config]}, use --force to override."
+    def init(filename=nil)
+      if filename.nil? || filename =~ /^\//
+        @config_filename = options[:config]
+      else
+        @config_filename = filename
+      end
+
+      config = Config.new(@config_filename)
+      if File.exists?(config.path) && !options[:force]
+        GithubCLI.ui.error "Not overwritting existing config file #{config.path}, use --force to override."
         exit 1
       end
 
-
+      oauth_token = ask "Please specify your GitHub Authentication Token (register on github.com to get it):"
+      config.save({'oauth_token' => oauth_token})
+      GithubCLI.ui.confirm "Writing new configuration file to #{config.path}"
     end
 
     desc 'ls <pattern>', 'List all available commands limited by pattern'
