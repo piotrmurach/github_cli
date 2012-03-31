ef989a8188605321276e7f9a8e4c2805d02c89ce
Piotr Murach <pmurach@gmail.com>
2012-03-31 16:36:47 +0100

    Add hooks.

---
+++ features/support/hooks.rb
@@ -0,0 +1,13 @@
+require 'fileutils'
+
+Before do
+  @real_home = ENV['HOME']
+  fake_home = File.join('/tmp', 'fakehome')
+  FileUtils.rm_rf fake_home if File.exists? fake_home
+  FileUtils.mkdir_p fake_home
+  ENV['HOME'] = fake_home
+end
+
+After do
+  ENV['HOME'] = @real_home
+end
