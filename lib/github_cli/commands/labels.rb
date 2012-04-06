8e40680d617741c8e68b4f5c3ae11441e1b1efbe
Piotr Murach <pmurach@gmail.com>
2012-04-06 18:26:51 +0100

    Remove command loading.

--- lib/github_cli/commands/labels.rb
+++ lib/github_cli/commands/labels.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-require 'github_cli/command'
-
 module GithubCLI
   class Commands::Labels < Command
 
--- lib/github_cli/commands/tags.rb
+++ lib/github_cli/commands/tags.rb
@@ -1,28 +1,25 @@
 # encoding: utf-8
 
 module GithubCLI
-  class Commands::Trees < Command
+  class Commands::Tags < Command
 
-    namespace :tree
+    namespace :tag
 
-    desc 'get <user> <repo> <sha>', 'Get a Tree'
+    desc 'get <user> <repo> <sha>', 'Get a Tag'
     method_option :recursive, :type => :boolean, :aliases => ["-r"],
                   :desc => 'get a tree recursively'
     method_option :params, :type => :hash, :default => {},
                   :desc => 'Additonal request parameters e.i per_page:100'
     def get(user, repo, sha)
-      if options[:recursive]
-        options[:params]['recursive'] = true
-      end
-      Tree.get user, repo, sha, options[:params]
+      Tag.get user, repo, sha, options[:params]
     end
 
-    desc 'create <user> <repo>', 'Create a new Tree'
+    desc 'create <user> <repo>', 'Create a Tag Object'
     method_option :params, :type => :hash, :default => {},
                   :desc => 'Additonal request parameters e.i per_page:100'
     def create(user, repo)
-      Tree.create user, repo, options[:params]
+      Tag.create user, repo, options[:params]
     end
 
-  end # Blobs
+  end # Tag
 end # GithubCLI
