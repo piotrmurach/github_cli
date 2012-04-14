22b580dcc612942492317741621d1381e74c72a4
Piotr Murach <pmurach@gmail.com>
2012-04-14 11:24:00 +0100

    Change to call tree methods.

--- lib/github_cli/apis/tree.rb
+++ lib/github_cli/apis/tree.rb
@@ -6,11 +6,11 @@ module GithubCLI
     class << self
 
       def get(user, repo, sha, params)
-        github_api.repos.get_blob user, repo, params
+        github_api.repos.get_tree user, repo, params
       end
 
       def create(user, repo, params)
-        github_api.repos.create_blob user, repo, params
+        github_api.repos.create_tree user, repo, params
       end
     end
 
