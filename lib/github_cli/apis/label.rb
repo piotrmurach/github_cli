0cdac378cb11b7a0cffa1e4b9b0fac7a27c2c023
Piotr Murach <pmurach@gmail.com>
2012-04-22 19:40:17 +0100

    Update labels commands and add api integration.

---
+++ lib/github_cli/apis/label.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+module GithubCLI
+  class Label < API
+
+    class << self
+
+      def all(params)
+        github_api.issues.labels params
+      end
+
+      def get(user, repo, name, params)
+        github_api.issues.get_label user, repo, name, params
+      end
+
+      def create(user, repo, params)
+        github_api.issues.create_label user, repo, params
+      end
+
+      def update(user, repo, name, params)
+        github_api.issues.update_label user, repo, params
+      end
+
+      def delete(user, repo, name, params)
+        github_api.issues.delete_label user, repo, name, params
+      end
+
+      def issue(user, repo, number, params)
+        github_api.issues.labels_for user, repo, number, params
+      end
+
+      def add(user, repo, number, *args)
+        github_api.issues.add_labels user, repo, args
+      end
+
+      def remove(user, repo, number, name=nil, params)
+        github_api,issues.remove_label user, repo, number, name, params
+      end
+
+      def replace(user, repo, number, *args)
+        github_api.issues.replace_labels user, repo, number, args
+      end
+
+      def milestone(user, repo, number, params)
+        gitub_api.issues.milestone_labels user, repo, number, params
+      end
+    end
+
+  end # Label
+end # GithubCLI
--- lib/github_cli/commands/labels.rb
+++ lib/github_cli/commands/labels.rb
@@ -5,30 +5,85 @@ module GithubCLI
 
     namespace :label
 
-    desc 'list', 'Listing all labels for this repository.'
-    def list
+    desc 'list <user> <repo>', 'Listing all labels for this repository.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def list(user, repo)
+      Label.all user, repo, options[:params]
     end
 
-    desc 'get <user>, <repo>', 'Get a single label.'
-    method_option :name, :type => :string, :required => true
-    def get(user, repo)
+    desc 'get <user> <repo> <name>', 'Get a single label.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def get(user, repo, name)
+      Label.get user, repo, name, options[:params]
     end
 
-    desc 'create <user>, <repo>', 'Create a label.'
-    method_option :params, :type => :hash
+    desc 'create <user> <repo>', 'Create a label.'
+    long_desc <<-DESC
+      Inputs
+        name - Required string
+        color - Required string - 6 character hex code, without leading #
+    DESC
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
     def create(user, repo)
+      Label.create user, repo, options[:params]
     end
 
-    method_option :name, :type => :string, :required => true
-    desc 'update <user>, <repo>', 'Update a label.'
-    method_option :params, :type => :hash
-    def update(user, repo)
+    desc 'update <user> <repo> <name>', 'Update a label.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def update(user, repo, name)
+      Label.update user, repo, name, options[:params]
     end
 
-    method_option :name, :type => :string, :required => true
-    desc 'delete <user>, <repo>', 'Delete a label.'
-    method_option :params, :type => :hash
-    def delete(user, repo)
+    desc 'delete <user> <repo> <name>', 'Delete a label.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def delete(user, repo, name)
+      Label.delete user, repo, name, options[:params]
+    end
+
+    desc 'issue <user> <repo> <number>', 'List labels on an issue.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def issue(user, repo, number)
+      Label.issue user, repo, number, options[:params]
+    end
+
+    desc 'add <user> <repo> <number>', 'Add labels to an issue.'
+    method_option :labels, :type => :string,
+                  :desc => 'Labels to be added to this issue.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additional request parameters e.i per_page:100'
+    def add(user, repo, number)
+      Label.add user, repo, number, options[:labels], options[:params]
+    end
+
+    desc 'remove <user> <repo> <number>', 'List labels on an issue.'
+    method_option :name, :type => :string, :default => nil,
+                  :desc => 'Label name'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def remove(user, repo, number)
+      Label.issue user, repo, number, options[:name], options[:params]
+    end
+
+    desc 'replace <user> <repo> <number>', 'Replace all labels for an issue.'
+    method_option :labels, :type => :string,
+                  :desc => 'Labels to be replace in this issue.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def replace(user, repo, number)
+      Label.replace user, repo, number, options[:labels], options[:params]
+    end
+
+    desc 'milestone <user> <repo> <number>', 'Get labels for every issue in a milestone.'
+    method_option :params, :type => :hash, :default => {},
+                  :desc => 'Additonal request parameters e.i per_page:100'
+    def milestone(user, repo, number)
+      Label.milestone user, repo, number, options[:params]
     end
 
   end # Labels
