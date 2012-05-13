94bc259457c414f68cc5f83f9e2d31bfede37a6d
Piotr Murach <pmurach@gmail.com>
2012-05-13 12:31:21 +0100

    Add exception types.

--- lib/github_cli/errors.rb
+++ lib/github_cli/errors.rb
@@ -1,6 +1,29 @@
-# Add cli errors here
+# encoding: utf-8
+
 module GithubCLI
 
-  class ConfigFileNotFound < StandardError; end
+  class GithubCLIError < StandardError
+    def self.status_code(code)
+      define_method(:status_code) { code }
+    end
+  end
+
+  # Raised when a configuration file is corrupt or missing.
+  class ConfigFileNotFound < GithubCLIError
+    status_code 10
+
+    def initialize
+      super %{Configuration file corrupt or missing}
+    end
+  end
+
+  # Raised when GitHub api raised error
+  class GitHubError < GithubCLIError
+    status_code 11
+  end
+
+  class HTTPError < GithubCLIError
+    status_code 12
+  end
 
 end # GithubCLI
