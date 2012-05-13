dd8a3d291f24cdce11538ee4b5bd718d148d4d38
Piotr Murach <pmurach@gmail.com>
2012-05-13 12:29:31 +0100

    Add specific types of exceptions.

--- lib/github_cli/api.rb
+++ lib/github_cli/api.rb
@@ -21,9 +21,33 @@ module GithubCLI
     def self.configure_api
     end
 
+    # TODO
+    # Will need to detect if all attribute names fit inside the terminal
+    # width, if not then rotate the table to display on each row. Try to
+    # create view like hirb.
+    # Finally all this needs to be moved to terminal class.
+
+    # output can take first parameter of type, whether :show, :table, :status
+    # status - would be a format to show only response from the request
+    # table  - used for listing more than one returned element
+    # show - shows details for a single resource
+
+    def self.output(type=:show, &block)
+      response =  block.call
+      case response
+      when Array
+        response.each do |item|
+          GithubCLI::Terminal.render_output item, :horizontal => true
+          puts
+        end
+      else
+        GithubCLI::Terminal.render_output response, :vertical => true
+      end
+    end
+
     class All
-      def initialize
-        github_api
+      def initialize(params)
+        puts Github::Repos.new.all params
       end
     end
 
