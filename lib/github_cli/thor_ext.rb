56ff10eefadc0a95e8cc6ef3d9680cf7ddc81f08
Piotr Murach <pmurach@gmail.com>
2012-05-28 22:09:24 +0100

    Customize commands display.

--- lib/github_cli/thor_ext.rb
+++ lib/github_cli/thor_ext.rb
@@ -4,16 +4,30 @@ class Thor
   include Thor::Base
 
   class << self
-     def subcommand_help(cmd)
-       desc "help <command>", "Describe subcommands or one specific subcommand"
-       class_eval <<-RUBY
-         def help(task = nil, subcommand = true); super; end
-        RUBY
-     end
+
+    def help(shell, subcommand = false)
+      list = printable_tasks(true, subcommand)
+      Thor::Util.thor_classes_in(self).each do |klass|
+        list += klass.printable_tasks(false)
+      end
+      list.sort!{ |a,b| a[0] <=> b[0] }
+
+      shell.say "Commands:"
+      shell.print_table(list, :indent => 2, :truncate => true)
+      shell.say
+      class_options_help(shell)
+    end
+
+    def subcommand_help(cmd)
+      desc "help <command>", "Describe subcommands or one specific subcommand"
+      class_eval <<-RUBY
+        def help(task = nil, subcommand = true); super; end
+      RUBY
+    end
   end
 
   desc "help <command>", "Describe available commands or one specific command"
   def help(task = nil, subcommand = false)
-      task ? self.class.task_help(shell, task) : self.class.help(shell, subcommand)
+    task ? self.class.task_help(shell, task) : self.class.help(shell, subcommand)
   end
 end # Thor
