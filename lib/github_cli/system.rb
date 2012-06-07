62ed23d43d2153ae0ba82130e87923bf2bf87654
Piotr Murach <pmurach@gmail.com>
2012-06-07 13:17:10 +0100

    Change the reference to process exec command.

--- lib/github_cli/system.rb
+++ lib/github_cli/system.rb
@@ -16,8 +16,8 @@ module GithubCLI
         exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
         ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
           exts.each do |ext|
-            exec = "#{path}/#{command}/#{ext}"
-            return exec if File.executable? exec
+            exe = "#{path}/#{command}#{ext}"
+            return exe if File.executable? exe
           end
         end
         return nil
