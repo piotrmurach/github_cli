737dccffa56bb50e5156308296acdf22022d7657
Piotr Murach <pmurach@gmail.com>
2012-06-13 11:00:20 +0100

    Change utility methods to be more robust, factor out value conversion.

--- lib/github_cli/util.rb
+++ lib/github_cli/util.rb
@@ -2,12 +2,13 @@ module GithubCLI
   module Util
     extend self
 
-    def flatten_hash(prefix=nil, hash, new_hash)
+    def flatten_hash(prefix=nil, hash)
+      new_hash ||= {}
       hash.each do |key, val|
         key =  prefix ? :"#{prefix}_#{key}" : key
         case val
         when Hash
-          flatten_hash(key, val, new_hash)
+          new_hash.update flatten_hash(key, val)
         else
           new_hash[key] = val
         end
@@ -19,16 +20,17 @@ module GithubCLI
       values_copy = values.dup
       collected = []
       values_copy.inject([]) do |collected, val|
-        collected << case val
-          when true
-            "true"
-          when false
-            "false"
-          when Hash
-            self.convert_values(val.values)
-          else
-            val.to_s
-          end
+        collected << convert_value(val)
+      end
+    end
+
+    def convert_value(value)
+      case value
+      when true  then "true"
+      when false then "false"
+      when Hash  then convert_value(value.values)
+      when Array then value.map(&:to_s)
+      else value.to_s
       end
     end
   end
--- spec/github_cli/util_spec.rb
+++ spec/github_cli/util_spec.rb
@@ -6,14 +6,12 @@ describe GithubCLI::Util do
 
     it 'preserves original hash' do
       hash = { :a => 1, :b => 2 }
-      output = {}
-      subject.flatten_hash hash, output
+      output = subject.flatten_hash hash
       output.should == hash
     end
 
     it "reduces multidimensional keys to one dimension" do
-      output = {}
-      subject.flatten_hash hash, output
+      output = subject.flatten_hash hash
       output.should == { :a_b_c => 1 }
     end
   end
@@ -32,6 +30,18 @@ describe GithubCLI::Util do
 
     it 'preserves strings' do
       subject.convert_values(values).should include 'string'
+    end
+  end
+
+  describe "#convert_value" do
+    it 'converts arrays to arrays of strings' do
+      values = [:bar, 123, 3.14]
+      subject.convert_value(values).should eq(["bar", "123", "3.14"])
+    end
+
+    it 'converts hash values to strings' do
+      values = {:foo => 123, :bar => :baz, :buz => 2.3}
+      subject.convert_value(values).should eq(["123", "baz", "2.3"])
     end
   end
 end # GithubCLI::Util
