ae2aeb505d5393670d902851f1079c8a68a4a837
Piotr Murach <pmurach@gmail.com>
2012-07-21 20:21:32 +0100

    Extend timeout for testing command output.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,9 @@
 require 'fileutils'
 
 Before do
-  @aruba_timeout_seconds = 10
+  @aruba_timeout_seconds = 5
+  @aruba_io_wait_seconds = 0.2
+
   @real_home = ENV['HOME']
   fake_home = File.join('/tmp', 'fakehome')
   FileUtils.rm_rf fake_home if File.exists? fake_home
