828385992a13261f05fdf1a9a9160f27383b83cd
Piotr Murach <pmurach@gmail.com>
2012-07-21 10:53:33 +0100

    Change to seek config file editor first, add proper exit codes.

--- lib/github_cli/editor.rb
+++ lib/github_cli/editor.rb
@@ -4,22 +4,27 @@ module GithubCLI
   # This class determines editor to use to open the output.
   class Editor
 
-    def initialize(options={})
+    attr_accessor :filename
+
+    def initialize(filename, options={})
+      @filename = filename
     end
 
     def editor
-      editors = [ ENV['GHC_EDITOR'], ENV['VISUAL'], ENV['EDITOR'], 'vi' ]
-      editor = GithubCLI.config['editor']
-      editor ||= editors.find { |e| !e.nil? && !e.empty? }
+      editors = [ ENV['GCLI_EDITOR'], ENV['VISUAL'], ENV['EDITOR'], 'vi' ]
+      editors.unshift(GithubCLI.config['core.editor'])
+      editors.compact.uniq.find { |editor| System.command? editor }
     end
 
-    def open(name)
+    def open(name=nil)
       if editor
         command = "#{editor} #{name}"
         success = system(command)
         GithubCLI.ui.info "Could not run '#{command}'" unless success
+        exit success
       else
         GithubCLI.info("To open output, set $EDITOR or $VISUL")
+        exit 1
       end
     end
 
