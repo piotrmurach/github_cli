a9974701ea5e7c80743aa8fcbf3780dfd35d09f0
Piotr Murach <pmurach@gmail.com>
2013-02-26 23:06:00 +0000

    Add tests for email api commands.

---
+++ spec/github_cli/commands/emails_spec.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Emails do
+  let(:format) { 'table' }
+  let(:user)   { 'peter-murach' }
+  let(:api_class) { GithubCLI::Email }
+
+  it "invokes email:list" do
+    api_class.should_receive(:all).with({}, format)
+    subject.invoke "email:list", []
+  end
+
+  it "invokes email:add" do
+    api_class.should_receive(:add).with(['email1', 'email2'], {}, format)
+    subject.invoke "email:add", ['email1', 'email2']
+  end
+
+  it "invokes email:delete" do
+    api_class.should_receive(:delete).with(['email1', 'email2'], {}, format)
+    subject.invoke "email:delete", ['email1', 'email2']
+  end
+end
