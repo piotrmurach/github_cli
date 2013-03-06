3452ce0e6ef4ab9f6373b9f6a03a0e0d4de47363
Piotr Murach <pmurach@gmail.com>
2013-03-06 23:26:17 +0000

    Add milestone commands specs.

---
+++ spec/github_cli/commands/milestones_spec.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Milestones do
+  let(:format) { 'table' }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:api_class) { GithubCLI::Milestone }
+
+  it "invokes milestone:list" do
+    api_class.should_receive(:all).with(user, repo, {}, format)
+    subject.invoke "milestone:list", [user, repo]
+  end
+
+  it "invokes milestone:get" do
+    api_class.should_receive(:get).with(user, repo, 1, {}, format)
+    subject.invoke "milestone:get", [user, repo, 1]
+  end
+
+  it "invokes milestone:create" do
+    api_class.should_receive(:create).with(user, repo, {}, format)
+    subject.invoke "milestone:create", [user, repo]
+  end
+
+  it "invokes milestone:update" do
+    api_class.should_receive(:update).with(user, repo, 1, {}, format)
+    subject.invoke "milestone:update", [user, repo, 1]
+  end
+
+  it "invokes milestone:delete" do
+    api_class.should_receive(:delete).with(user, repo, 1, {}, format)
+    subject.invoke "milestone:delete", [user, repo, 1]
+  end
+end
