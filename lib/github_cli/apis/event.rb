3ec8a3599f00bcfcc4e1655c754da0276cb961c3
Piotr Murach <pmurach@gmail.com>
2013-03-14 23:16:42 +0000

    Change events api to use activity scope.

--- lib/github_cli/apis/event.rb
+++ lib/github_cli/apis/event.rb
@@ -7,49 +7,49 @@ module GithubCLI
 
       def public(params, format)
         output format do
-          github_api.events.public params
+          github_api.activity.events.public params
         end
       end
 
       def repository(user, repo, params, format)
         output format do
-          github_api.events.repository user, repo, params
+          github_api.activity.events.repository user, repo, params
         end
       end
 
       def issue(user, repo, params, format)
         output format do
-          github_api.events.issue user, repo, params
+          github_api.activity.events.issue user, repo, params
         end
       end
 
       def network(user, repo, params, format)
         output format do
-          github_api.events.network user, repo, params
+          github_api.activity.events.network user, repo, params
         end
       end
 
       def organization(org, params, format)
         output format do
-          github_api.events.organization org, params
+          github_api.activity.events.organization org, params
         end
       end
 
       def received(user, params, format)
         output format do
-          github_api.events.received user, params
+          github_api.activity.events.received user, params
         end
       end
 
       def performed(user, params, format)
         output format do
-          github_api.events.performed user, params
+          github_api.activity.events.performed user, params
         end
       end
 
       def user_org(user, org, params, format)
         output format do
-          github_api.events.user_org user, org, params
+          github_api.activity.events.user_org user, org, params
         end
       end
     end
