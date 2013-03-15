cd45bf8f55b6f028de18b36ad0e62d3c509d65c0
Piotr Murach <pmurach@gmail.com>
2013-03-15 23:23:21 +0000

    Change starring api endpoint to use activity scope.

--- lib/github_cli/apis/starring.rb
+++ lib/github_cli/apis/starring.rb
@@ -7,31 +7,31 @@ module GithubCLI
 
       def list(user, repo, params, format)
         output format do
-          github_api.repos.starring.list user, repo, params
+          github_api.activity.starring.list user, repo, params
         end
       end
 
       def starred(params, format)
         output format do
-          github_api.repos.starring.starred params
+          github_api.activity.starring.starred params
         end
       end
 
       def starring?(user, repo, params, format)
         output format do
-          github_api.repos.starring.starring? user, repo, params
+          github_api.activity.starring.starring? user, repo, params
         end
       end
 
       def star(user, repo, params, format)
         output format do
-          github_api.repos.starring.star user, repo, params
+          github_api.activity.starring.star user, repo, params
         end
       end
 
       def unstar(user, repo, params, format)
         output format do
-          github_api.repos.starring.unstar user, repo, params
+          github_api.activity.starring.unstar user, repo, params
         end
       end
     end
