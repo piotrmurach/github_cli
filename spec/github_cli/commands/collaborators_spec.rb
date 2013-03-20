8d67c289dd9e14124a143d5b3885b5003ce0200a
Piotr Murach <pmurach@gmail.com>
2013-03-20 22:39:30 +0000

    Add collaborator commands spec.

---
+++ spec/github_cli/commands/collaborators_spec.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Collaborators do
+  let(:format) { 'table' }
+  let(:owner)  { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:user)   { 'github' }
+  let(:api_class) { GithubCLI::Collaborator }
+
+  it "invokes collab:list" do
+    api_class.should_receive(:all).with(owner, repo, {}, format)
+    subject.invoke "collab:list", [owner, repo]
+  end
+
+  it "invokes collab:add" do
+    api_class.should_receive(:add).with(owner, repo, user, {}, format)
+    subject.invoke "collab:add", [owner, repo, user]
+  end
+
+  it "invokes collab:collab" do
+    api_class.should_receive(:collaborator?).with(owner, repo, user, {}, format)
+    subject.invoke "collab:collab", [owner, repo, user]
+  end
+
+  it "invokes collab:collab" do
+    api_class.should_receive(:remove).with(owner, repo, user, {}, format)
+    subject.invoke "collab:remove", [owner, repo, user]
+  end
+end
