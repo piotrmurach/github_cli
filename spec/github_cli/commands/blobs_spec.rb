d80d3d12a59db02ea40bb79b6fc01aa19aef218e
Piotr Murach <pmurach@gmail.com>
2013-03-20 22:49:09 +0000

    Add blob commands specs.

---
+++ spec/github_cli/commands/blobs_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Blobs do
+  let(:format) { 'table' }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:sha)    { '3a0f86fb8db8eea7ccbb9a95f325ddbedfb25e15' }
+  let(:api_class) { GithubCLI::Blob }
+
+  it "invokes blob:get" do
+    api_class.should_receive(:get).with(user, repo, sha, {}, format)
+    subject.invoke "blob:get", [user, repo, sha]
+  end
+
+  it "invokes blob:create" do
+    api_class.should_receive(:create).with(user, repo, {}, format)
+    subject.invoke "blob:create", [user, repo]
+  end
+end
