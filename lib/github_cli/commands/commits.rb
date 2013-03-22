d1dc8c49b69b4682b6287192c153fea50aecb291
Piotr Murach <pmurach@gmail.com>
2013-03-22 23:25:11 +0000

    Change to take specific options.

--- lib/github_cli/commands/commits.rb
+++ lib/github_cli/commands/commits.rb
@@ -11,6 +11,16 @@ module GithubCLI
     end
 
     desc 'create <user> <repo>', 'Create a Commit'
+    option :message, :type => :string, :aliases => ["-m"],
+      :desc => "String of the commit message."
+    option :tree, :type => :string, :aliases => ["-t"], :banner => "<sha>",
+      :desc => "String of the SHA of the tree object this commit points to."
+    option :parents, :type => :array, :banner => "<sha1> <sha2> ...",
+      :desc => "Array of the SHAs of the commits that were the parents of this commit. "
+    option :author, :type => :hash,
+      :desc => "If the author section is omitted, it will be filled in with the authenticated user’s information and the current date."
+    option :committer, :type => :hash,
+      :desc => "The committer section is optional and will be filled with the author data if omitted."
     long_desc <<-DESC
       Parameters
 
@@ -30,7 +40,13 @@ module GithubCLI
         committer.date - Timestamp of when this commit was committed
     DESC
     def create(user, repo)
-      Commit.create user, repo, options[:params], options[:format]
+      params = options[:params].dup
+      params['message']   = options[:message] if options[:message]
+      params['tree']      = options[:tree] if options[:tree]
+      params['parents']   = options[:parents] if options[:parents]
+      params['author']    = options[:author] if options[:author]
+      params['committer'] = options[:committer] if options[:committer]
+      Commit.create user, repo, params, options[:format]
     end
 
   end # Commit
