63758fa3ce1c7bdab94e672f90d376a94ac59a53
Piotr Murach <pmurach@gmail.com>
2013-03-23 14:43:12 +0000

    Change to include specific options.

--- lib/github_cli/commands/milestones.rb
+++ lib/github_cli/commands/milestones.rb
@@ -6,6 +6,12 @@ module GithubCLI
     namespace :milestone
 
     desc 'list <user> <repo>', 'List milestones for a repository'
+    option :state, :type => :string, :default => "open",
+      :desc => "open, closed, default: open"
+    option :sort, :type => :string, :default => "due_date",
+      :desc => "due_date, completeness, default: due_date"
+    option :direction, :type => :string, :default => "desc",
+      :desc => "asc, desc, default: desc"
     long_desc <<-DESC
       Parameters
 
@@ -14,7 +20,11 @@ module GithubCLI
         direction - asc, desc, default: desc\n
     DESC
     def list(user, repo)
-      Milestone.all user, repo, options[:params], options[:format]
+      params = options[:params].dup
+      params['state'] = options[:state] || 'open'
+      params['sort']  = options[:sort] || 'due_date'
+      params['direction'] = options[:direction] || 'desc'
+      Milestone.all user, repo, params, options[:format]
     end
 
     desc 'get <user> <repo> <number>', 'Get a single milestone'
@@ -23,6 +33,11 @@ module GithubCLI
     end
 
     desc 'create <user> <repo>', 'Create a milestone'
+    option :title, :type => :string, :required => true
+    option :state, :type => :string, :default => "open",
+      :desc => "open, closed, default: open"
+    option :desc, :type => :string
+    option :due_on, :type => :string, :desc => "Optional string - ISO 8601 time"
     long_desc <<-DESC
       Parameters
 
@@ -33,13 +48,23 @@ module GithubCLI
 
       Example
 
-      ghc milestone create wycats thor --params=title:new
+      ghc milestone create wycats thor --title=new
     DESC
     def create(user, repo)
-      Milestone.create user, repo, options[:params], options[:format]
+      params = options[:params].dup
+      params['title'] = options[:title]
+      params['state'] = options[:state] if options[:state]
+      params['description'] = options[:desc] if options[:desc]
+      params['due_on'] = options[:due_on] if options[:due_on]
+      Milestone.create user, repo, params, options[:format]
     end
 
     desc 'update <user> <repo> <number>', 'Update a milestone'
+    option :title, :type => :string
+    option :state, :type => :string, :default => "open",
+      :desc => "open, closed, default: open"
+    option :desc, :type => :string
+    option :due_on, :type => :string, :desc => "Optional string - ISO 8601 time"
     long_desc <<-DESC
       Parameters
 
@@ -50,10 +75,15 @@ module GithubCLI
 
       Example
 
-      ghc milestone update wycats thor 1 --params=title:new
+      ghc milestone update wycats thor 1 --title=new
     DESC
     def update(user, repo, number)
-      Milestone.update user, repo, number, options[:params], options[:format]
+      params = options[:params].dup
+      params['title'] = options[:title] if options[:title]
+      params['state'] = options[:state] if options[:state]
+      params['description'] = options[:desc] if options[:desc]
+      params['due_on'] = options[:due_on] if options[:due_on]
+      Milestone.update user, repo, number, params, options[:format]
     end
 
     desc 'delete <user> <repo> <number>', 'Delete a milestone'
