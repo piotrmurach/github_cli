ee2e7088c3f754f993612102efe8de877ae6528c
Piotr Murach <pmurach@gmail.com>
2013-03-23 13:32:58 +0000

    Add tag commands specs.

---
+++ spec/github_cli/commands/tags_spec.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Tags do
+  let(:format) { 'table' }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:sha)    { '3a0f86fb8db8eea7ccbb9a95f325ddbedfb25e15' }
+  let(:api_class) { GithubCLI::Tag }
+
+  it "invokes tag:get" do
+    api_class.should_receive(:get).with(user, repo, sha, {}, format)
+    subject.invoke "tag:get", [user, repo, sha]
+  end
+
+  it "invokes tag:create" do
+    api_class.should_receive(:create).with(user, repo, {}, format)
+    subject.invoke "tag:create", [user, repo]
+  end
+
+  it "invokes tag:create --object" do
+    api_class.should_receive(:create).with(user, repo, {'object' => sha}, format)
+    subject.invoke "tag:create", [user, repo], :object => sha
+  end
+end
