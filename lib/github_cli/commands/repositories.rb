f95915ff60e34a235c987791c207098b4969dffe
Piotr Murach <pmurach@gmail.com>
2013-03-29 23:55:19 +0000

    Add specific options to repo create command.

--- lib/github_cli/commands/repositories.rb
+++ lib/github_cli/commands/repositories.rb
@@ -6,17 +6,17 @@ module GithubCLI
     namespace :repo
 
     option :org, :type => :string, :aliases => ["-o"], :banner => '<organization>',
-      :desc => 'List repositories for <organization>'
+           :desc => 'List repositories for <organization>'
     option :user, :type => :string, :aliases => ["-u"], :banner => '<user>',
-      :desc => 'List repositories for <user>'
+           :desc => 'List repositories for <user>'
     option :since, :type => :string, :banner => '<id>',
-      :desc => "the integer ID of the last Repository that you've seen"
+           :desc => "the integer ID of the last Repository that you've seen"
     option :type, :type => :string,
-      :desc => "all, owner, public, private, member. Default: all."
+           :desc => "all, owner, public, private, member. Default: all."
     option :sort, :type => :string,
-      :desc => "created, updated, pushed, full_name, default: full_name."
+           :desc => "created, updated, pushed, full_name, default: full_name."
     option :direction, :type => :string,
-      :desc => "asc or desc, default: when using full_name: asc, otherwise desc."
+           :desc => "asc or desc, default: when using full_name: asc, otherwise desc."
     desc 'list', 'Lists all repositories for the authenticated user'
     def list
       params = options[:params].dup
@@ -34,6 +34,25 @@ module GithubCLI
       Repository.get user, repo, options[:params], options[:format]
     end
 
+    option :org, :type => :string, :aliases => ["-o"],
+           :desc => 'Create repository in <organization>',
+           :banner => '<organization>'
+    option :desc, :type => :string, :banner => "description"
+    option :home, :type => :string, :banner => "homepage"
+    option :private, :type => :boolean, 
+           :desc => "true to create a private repository, false to create a public one"
+    option :issues, :type => :boolean, :banner => "has_issues",
+           :desc => "true to enable issues for this repository, false to disable them"
+    option :wiki, :type => :boolean, :banner => "has_wiki",
+           :desc => "true to enable the wiki for this repository, false to disable it. Default is true"
+    option :downloads, :type => :boolean, :banner => "has_downloads",
+           :desc => "true to enable downloads for this repository "
+    option :team, :type => :string, :banner => "team <id>",
+           :desc => "The id of the team that will be granted access to this repository. This is only valid when creating a repo in an organization"
+    option :auto, :type => :string, :banner => "auto_init",
+           :desc => "true to create an initial commit with empty README. Default is false."
+    option :gitignore, :type => :string, :banner => "gitignore_template",
+           :desc => "Desired language or platform .gitignore template to apply."
     desc 'create [<org>/]<name>', 'Create a new repository <name> for the authenticated user.'
     long_desc <<-DESC
       Create a new repository for the autheticated user.
@@ -48,16 +67,24 @@ module GithubCLI
         has_downloads Optional boolean - true to enable downloads for this repository \n
         org Optional string - The organisation in which this repository will be created \n
         team_id - Optional number - The id of the team that will be granted access to this repository. This is only valid when creating a repo in an organization \n
+        auto_init - Optional boolean - true to create an initial commit with empty README. Default is false.
+        gitignore_template - Optional string - Desired language or platform .gitignore template to apply. Use the name of the template without the extension. For example, “Haskell” Ignored if auto_init parameter is not provided.
     DESC
-    method_option :org, :type => :string, :aliases => ["-o"],
-                  :desc => 'Create repository in <organization>',
-                  :banner => '<organization>'
     def create(args)
-      org, options[:params]['name'] = Arguments.new(args).parse
-      options[:params]['org'] = org if org
-      options[:params]['org'] = options[:org] if options[:org]
+      params = options[:params].dup
+      org, params['name'] = Arguments.new(args).parse
+      params['org'] = org if org
+      params['org'] = options[:org] if options[:org]
+      params['description'] = options[:desc] if options[:desc]
+      params['homepage'] = options[:home] if options[:home]
+      params['private'] = options[:private] if options[:private]
+      params['has_issues'] = options[:issues] if options[:issues]
+      params['has_wiki'] = options[:wiki] if options[:wiki]
+      params['has_downloads'] = options[:downloads] if options[:downloads]
+      params['team_id'] = options[:team] if options[:team]
+      params['auto_init'] = options[:auto] if options[:auto]
 
-      Repository.create options[:params], options[:format]
+      Repository.create params, options[:format]
     end
 
     desc 'edit <user> <repo>', 'Edit <repo> for an <user>.'
