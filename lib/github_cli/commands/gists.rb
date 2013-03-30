13532c1ba335bbd7ea4e7657a9bf87eaf2977326
Piotr Murach <pmurach@gmail.com>
2013-03-30 22:14:25 +0000

    Add gist commands specific options.

--- lib/github_cli/commands/gists.rb
+++ lib/github_cli/commands/gists.rb
@@ -6,20 +6,19 @@ module GithubCLI
     namespace :gist
 
     desc 'list', 'List all gists'
-    method_option :user, :type => :string, :aliases => ["-u"],
-                  :desc => 'List a <user> gists',
-                  :banner => '<user>'
-    method_option :starred, :type => :boolean, :aliases => ["-s"],
-                  :default => false,
-                  :desc => 'List the authenticated users starred gists'
+    option :user, :type => :string, :aliases => ["-u"], :banner => '<user>',
+           :desc => 'List a <user> gists'
+    option :starred, :type => :boolean, :aliases => ["-s"], :default => false,
+           :desc => 'List the authenticated users starred gists'
+    option :since, :type => :string, :banner => "timestamp",
+           :desc => "a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ "
     def list
       if options[:starred]
         Gist.starred options[:params], options[:format]
       else
-        if options[:user]
-          options[:params]['user'] = options[:user]
-        end
-        Gist.all options[:params], options[:format]
+        params = options[:params].dup
+        params['user'] = options[:user] if options[:user]
+        Gist.all params, options[:format]
       end
     end
 
@@ -28,6 +27,10 @@ module GithubCLI
       Gist.get id, options[:params], options[:format]
     end
 
+    option :public, :type => :boolean, :default => false
+    option :desc, :type => :string
+    option :files, :type => :hash, :banner => "file1.txt:",
+           :desc => "Files that make up this gist. The key of which should be a required string filename and the value another required hash with parameters"
     desc 'create', 'Create a gist'
     long_desc <<-DESC
       Create a gist
@@ -40,9 +43,16 @@ module GithubCLI
           content - Required string - File contents.
     DESC
     def create
-      Gist.create options[:params], options[:format]
+      params = options[:params].dup
+      params['description'] = options[:desc] if options[:desc]
+      params['public'] = options[:public] || false
+      params['files'] = options[:files] if options[:files]
+      Gist.create params, options[:format]
     end
 
+    option :desc, :type => :string
+    option :files, :type => :hash, :banner => "file1.txt:",
+           :desc => "Files that make up this gist. The key of which should be a required string filename and the value another required hash with parameters"
     desc 'edit <id>', 'Edit a gist'
     long_desc <<-DESC
       Edit a gist
@@ -55,7 +65,10 @@ module GithubCLI
           filename - Optional string - New name for this file.\n
     DESC
     def edit(id)
-      Gist.edit id, options[:params], options[:format]
+      params = options[:params].dup
+      params['description'] = options[:desc] if options[:desc]
+      params['files'] = options[:files] if options[:files]
+      Gist.edit id, params, options[:format]
     end
 
     desc 'star <id>', 'Star a gist'
