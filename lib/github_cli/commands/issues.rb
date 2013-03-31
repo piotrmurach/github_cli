f35938ce131dfd6fa1af151bd4d649e0a66ffe3c
Piotr Murach <pmurach@gmail.com>
2013-03-31 23:37:15 +0100

    Add specific options to issue commands.

--- lib/github_cli/commands/issues.rb
+++ lib/github_cli/commands/issues.rb
@@ -5,9 +5,25 @@ module GithubCLI
 
     namespace :issue
 
-    desc 'list', 'List all issues'
+
+    option :filter, :type => :string,
+           :banner => "assigned|created|mentioned|subscribed|all"
+    option :state, :type => :string, :banner => "open|closed",
+           :desc => "open, closed, default: open"
+    option :labels, :type => :array,
+           :desc => "list of comma separated Label names. Example: bug,ui,@high"
+    option :sort, :type => :string, :banner => "created|updated|comments",
+           :desc => "created, updated, comments, default: created"
+    option :direction, :type => :string, :banner => "asc|desc",
+           :desc => "asc or desc, default: desc"
+    option :since, :type => :string,
+           :desc => "string of a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ"
+    option :org, :type => :string, :aliases => ["-o"]
+    option :user, :type => :string, :aliases => ["-u"]
+    option :repo, :type => :string, :aliases => ["-r"]
+    desc 'list', 'List issues'
     long_desc <<-DESC
-      ghc issue list --params filter:'assigned', state:'open'
+      ghc issue list --filter=assigned --state=open
 
       Parameters
 
@@ -23,7 +39,18 @@ module GithubCLI
         since - Optional string of a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ \n
     DESC
     def list
-      Issue.all options[:params], options[:format]
+      params = options[:params].dup
+      params['org'] = options[:org] if options[:org]
+      params['user'] = options[:user] if options[:user]
+      params['repo'] = options[:repo] if options[:repo]
+      params['filter'] = options[:filter] if options[:filter]
+      params['sort'] = options[:sort] if options[:sort]
+      params['state'] = options[:state] if options[:state]
+      params['labels'] = options[:labels] if options[:labels]
+      params['direction'] = options[:direction] if options[:direction]
+      params['since'] = options[:since] if options[:since]
+
+      Issue.all params, options[:format]
     end
 
     desc 'repo <user> <repo>', 'List all issues for a repository'
@@ -49,11 +76,19 @@ module GithubCLI
       Issue.all_repo user, repo, options[:params], options[:format]
     end
 
-    desc 'get <user> <repo> <issue-id>', 'Get a single issue'
-    def get(user, repo, id)
-      Issue.get user, repo, id, options[:params], options[:format]
+    desc 'get <user> <repo> <number>', 'Get a single issue'
+    def get(user, repo, number)
+      Issue.get user, repo, number, options[:params], options[:format]
     end
 
+    option :title, :type => :string, :required => true
+    option :body, :type => :string
+    option :assignee, :type => :string,
+           :desc => "Login for the user that this issue should be assigned to."
+    option :milestone, :type => :numeric,
+           :desc => "Milestone to associate this issue with."
+    option :labels, :type => :array,
+           :desc => "Labels to associate with this issue."
     desc 'create <user> <repo>', 'Create an issue.'
     long_desc <<-DESC
       Parameters
@@ -66,13 +101,30 @@ module GithubCLI
 
       Example
 
-      ghc issue create wycats thor --params=title:'Found a bug'
+      ghc issue create wycats thor --title='Found a bug'
     DESC
     def create(user, repo)
-      Issue.create user, repo, options[:params], options[:format]
+      params = options[:params].dup
+      params['title'] = options[:title]
+      params['body'] = options[:body] if options[:body]
+      params['assignee'] = options[:assignee] if options[:assignee]
+      params['milestone'] = options[:milestone] if options[:milestone]
+      params['labels'] = options[:labels] if options[:labels]
+
+      Issue.create user, repo, params, options[:format]
     end
 
-    desc 'edit <user> <repo> <issue-id>', 'Edit an issue.'
+    option :title, :type => :string, :required => true
+    option :body, :type => :string
+    option :assignee, :type => :string,
+           :desc => "Login for the user that this issue should be assigned to."
+    option :milestone, :type => :string,
+           :desc => "Milestone to associate this issue with."
+    option :labels, :type => :array,
+           :desc => "Labels to associate with this issue."
+    option :state, :type => :string, :banner => "open|closed",
+           :desc => "open, closed, default: open"
+    desc 'edit <user> <repo> <number>', 'Edit an issue.'
     long_desc <<-DESC
       Parameters
 
@@ -85,10 +137,18 @@ module GithubCLI
 
       Example
 
-      ghc issue edit wycats thor 1 --params=title:'Found a bug'
+      ghc issue edit wycats thor 1 --title='Found a bug'
     DESC
-    def edit(user, repo, id)
-      Issue.edit user, repo, id, options[:params], options[:format]
+    def edit(user, repo, number)
+      params = options[:params].dup
+      params['title'] = options[:title]
+      params['body'] = options[:body] if options[:body]
+      params['assignee'] = options[:assignee] if options[:assignee]
+      params['milestone'] = options[:milestone] if options[:milestone]
+      params['labels'] = options[:labels] if options[:labels]
+      params['state'] = options[:state] if options[:state]
+
+      Issue.edit user, repo, number, params, options[:format]
     end
 
   end # Issues
