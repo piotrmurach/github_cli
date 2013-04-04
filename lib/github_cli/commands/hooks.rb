921bbe1ffaf8f04522c0b81106d6894fd9a09f5d
Piotr Murach <pmurach@gmail.com>
2013-04-04 22:31:01 +0100

    Add hook commands specific options.

--- lib/github_cli/commands/hooks.rb
+++ lib/github_cli/commands/hooks.rb
@@ -5,7 +5,7 @@ module GithubCLI
 
     namespace :hook
 
-    desc 'list <user> <repo>', 'Lists hooks'
+    desc 'list <user> <repo>', 'List repository hooks'
     def list(user, repo)
       Hook.all user, repo, options[:params], options[:format]
     end
@@ -15,6 +15,14 @@ module GithubCLI
       Hook.get user, repo, id, options[:params], options[:format]
     end
 
+    option :name, :type => :string, :required => true, :banner => "service",
+           :desc => "the name of the service that is being called"
+    option :config, :type => :hash, :required => :true, :banner => "",
+           :desc => "a hash containing key/value pairs to provide settings for this hook"
+    option :events, :type => :array,
+           :desc => "Determines what events the hook is triggered for. Default: ['push']"
+    option :active, :type => :boolean, :default => true,
+           :desc => "determines whether the hook is actually triggered on pushes"
     desc 'create <user> <repo>', 'Create a hook'
     long_desc <<-DESC
       Inputs
@@ -25,9 +33,27 @@ module GithubCLI
       active - Optional boolean - Determines whether the hook is actually triggered on pushes.
     DESC
     def create(user, repo)
-      Hook.create user, repo, options[:params], options[:format]
+      params = options[:params].dup
+      params['name']   = options[:name]
+      params['config'] = options[:config]
+      params['events'] = options[:events] if options[:events]
+      params['active'] = options[:active] if options[:active]
+
+      Hook.create user, repo, params, options[:format]
     end
 
+    option :name, :type => :string, :required => true, :banner => "service",
+           :desc => "the name of the service that is being called"
+    option :config, :type => :hash, :required => :true, :banner => "",
+           :desc => "a hash containing key/value pairs to provide settings for this hook"
+    option :events, :type => :array,
+           :desc => "Determines what events the hook is triggered for. Default: ['push']"
+    option :add_events, :type => :array,
+           :desc => "Determines a list of events to be added to the list of events that the Hook triggers for."
+    option :remove_events, :type => :array,
+           :desc => "Determines a list of events to be removed from the list of events that the Hook triggers for."
+    option :active, :type => :boolean, :default => true,
+           :desc => "determines whether the hook is actually triggered on pushes"
     desc 'edit <user> <repo> <id>', 'Edit a hook'
     long_desc <<-DESC
       Inputs
@@ -40,7 +66,15 @@ module GithubCLI
       active - Optional boolean - Determines whether the hook is actually triggered on pushes. \n
     DESC
     def edit(user, repo, id)
-      Hook.edit user, repo, id, options[:params], options[:format]
+      params = options[:params].dup
+      params['name']          = options[:name]
+      params['config']        = options[:config]
+      params['events']        = options[:events]        if options[:events]
+      params['add_events']    = options[:add_events]    if options[:add_events]
+      params['remove_events'] = options[:remove_events] if options[:remove_events]
+      params['active']        = options[:active]        if options[:active]
+
+      Hook.edit user, repo, id, params, options[:format]
     end
 
     desc 'test <user> <repo> <id>', 'Test a hook'
