f1f6b3cb47c1165a1586cbd6c5cffd61ec2e6459
Piotr Murach <pmurach@gmail.com>
2013-04-14 21:17:07 +0100

    Add specific options to merge command.

--- lib/github_cli/commands/merging.rb
+++ lib/github_cli/commands/merging.rb
@@ -5,6 +5,12 @@ module GithubCLI
 
     namespace :merge
 
+    option :base, :type => :string, :required => true,
+           :desc => "The name of the base branch that the head will be merged into"
+    option :head, :type => :string, :required => true,
+           :desc => "The head to merge. This can be a branch name or a commit SHA1"
+    option :message, :type => :string,
+           :desc => "Commit message to use for the merge commit"
     desc 'perform <user> <repo>', 'Perform merge'
     long_desc <<-DESC
       Inputs
@@ -14,7 +20,12 @@ module GithubCLI
       commit_message - Optional String - Commit message to use for the merge commit. If omitted, a default message will be used.\n
     DESC
     def perform(user, repo)
-      Merging.merge user, repo, options[:params], options[:format]
+      params = options[:params]
+      params['base'] = options[:base] if options[:base]
+      params['head'] = options[:head] if options[:head]
+      params['commit_message'] = options[:message] if options[:message]
+
+      Merging.merge user, repo, params, options[:format]
     end
 
   end # Merging
