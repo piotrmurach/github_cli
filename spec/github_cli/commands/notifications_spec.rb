33acb66dcefa9f3b17f7bafb3f006e5964d72a1b
Piotr Murach <pmurach@gmail.com>
2013-04-14 21:05:36 +0100

    Add specs for notify command.

--- spec/github_cli/commands/notifications_spec.rb
+++ spec/github_cli/commands/notifications_spec.rb
@@ -23,4 +23,35 @@ describe GithubCLI::Commands::Notifications do
     api_class.should_receive(:get).with(id, {}, format)
     subject.invoke "notify:get", [id]
   end
+
+  it "invokes notify:mark" do
+    api_class.should_receive(:mark).with({'read'=>true}, format)
+    subject.invoke "notify:mark", [], :read => true
+  end
+
+  it "invokes notify:mark --id" do
+    api_class.should_receive(:mark).with({'thread_id'=>id,'read'=>true}, format)
+    subject.invoke "notify:mark", [], :id => id, :read => true
+  end
+
+  it "invokes notify:mark --user --repo" do
+    api_class.should_receive(:mark).with({'user'=>user,'repo'=>repo}, format)
+    subject.invoke "notify:mark", [], :user => user, :repo => repo
+  end
+
+  it "invokes notify:check" do
+    api_class.should_receive(:subscribed?).with(id, {}, format)
+    subject.invoke "notify:check", [id]
+  end
+
+  it "invokes notify:create" do
+    api_class.should_receive(:create).with(id, {}, format)
+    subject.invoke "notify:create", [id]
+  end
+
+  it "invokes notify:delete" do
+    api_class.should_receive(:delete).with(id, {}, format)
+    subject.invoke "notify:delete", [id]
+  end
+
 end
