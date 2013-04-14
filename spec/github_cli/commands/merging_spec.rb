438f8c2f8bbf4f2f1beff85bf0449aad36589102
Piotr Murach <pmurach@gmail.com>
2013-04-14 21:19:18 +0100

    Add specs for merge command.

---
+++ spec/github_cli/commands/merging_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Merging do
+  let(:format) { 'table' }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:id)     { 1 }
+  let(:api_class) { GithubCLI::Merging }
+
+  it "invokes merge:perform" do
+    api_class.should_receive(:merge).with(user, repo, {'base'=>'master', 'head'=>'feature'}, format)
+    subject.invoke "merge:perform", [user, repo], :base => 'master', :head => 'feature'
+  end
+
+end
