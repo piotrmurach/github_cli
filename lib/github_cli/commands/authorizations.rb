fd7f62bf08531adeb6508008a67d5bbe0f4e9fb7
Piotr Murach <pmurach@gmail.com>
2013-04-15 23:22:25 +0100

    Pass global options through.

--- lib/github_cli/commands/authorizations.rb
+++ lib/github_cli/commands/authorizations.rb
@@ -10,7 +10,8 @@ module GithubCLI
       You can only list your own tokens, and only through Basic Authentication.
     DESC
     def list
-      Authorization.all options[:params], options[:format]
+      params = options[:params].dup
+      Authorization.all params, options
     end
 
     desc 'get <id>', 'Get a single authorization'
@@ -18,7 +19,8 @@ module GithubCLI
       You can only access your own token, and only through Basic Authentication.
     DESC
     def get(id)
-      Authorization.get id, options[:params], options[:format]
+      params = options[:params].dup
+      Authorization.get id, params, options
     end
 
     desc 'create', 'Create a new authorization'
@@ -42,7 +44,8 @@ module GithubCLI
       params['scopes']   = options[:scopes]   if options[:scopes]
       params['note']     = options[:note]     if options[:note]
       params['note_url'] = options[:note_url] if options[:note_url]
-      Authorization.create params, options[:format]
+
+      Authorization.create params, options
     end
 
     desc 'update <id>', 'Update an existing authorization'
@@ -72,12 +75,13 @@ module GithubCLI
       params['remove_scopes'] = options[:remove_scopes] if options[:remove_scopes]
       params['note']          = options[:note]          if options[:note]
       params['note_url']      = options[:note_url]      if options[:note_url]
-      Authorization.update id, params, options[:format]
+      Authorization.update id, params, options
     end
 
     desc 'delete <id>', 'Delete an authorization'
     def delete(id)
-      Authorization.delete id, options[:params], options[:format]
+      params = options[:params].dup
+      Authorization.delete id, params, options
     end
 
   end # Authorizations
