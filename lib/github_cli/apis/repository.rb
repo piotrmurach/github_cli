11de7a4d01c2efa2bb43c4631c6e63fc4c3c55d4
Piotr Murach <pmurach@gmail.com>
2013-04-17 23:19:55 +0100

    Change repository apis to take extra options

--- lib/github_cli/apis/repository.rb
+++ lib/github_cli/apis/repository.rb
@@ -5,69 +5,69 @@ module GithubCLI
 
     class << self
 
-      def all(params, format)
-        output format do
-          github_api.repos.list params
+      def all(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.list params
         end
       end
 
-      def get(user, repo, params, format)
-        output format do
-          github_api.repos.get user, repo, params
+      def get(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.get user, repo, params
         end
       end
 
-      def create(params, format)
-        output format do
-          github_api.repos.create params
+      def create(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.create params
         end
       end
 
-      def edit(user, repo, params, format)
-        output format do
-          github_api.repos.edit user, repo, params
+      def edit(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.edit user, repo, params
         end
       end
 
-      def delete(user, repo, params, format)
-        output format do
-          github_api.repos.delete user, repo, params
+      def delete(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.delete user, repo, params
         end
       end
 
-      def branches(user, repo, params, format)
-        output format do
-          github_api.repos.branches user, repo, params
+      def branches(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.branches user, repo, params
         end
       end
 
-      def branch(user, repo, name, params, format)
-        output format do
-          github_api.repos.branch user, repo, name, params
+      def branch(user, repo, name, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.branch user, repo, name, params
         end
       end
 
-      def contributors(user, repo, params, format)
-        output format do
-          github_api.repos.contributors user, repo, params
+      def contributors(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.contributors user, repo, params
         end
       end
 
-      def languages(user, repo, params, format)
-        output format do
-          github_api.repos.languages user, repo, params
+      def languages(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.languages user, repo, params
         end
       end
 
-      def tags(user, repo, params, format)
-        output format do
+      def tags(user, repo, params, options)
+        output options[:format], options[:quiet] do
           github_api.repos.tags user, repo, params
         end
       end
 
-      def teams(user, repo, params, format)
-        output format do
-          github_api.repos.teams user, repo, params
+      def teams(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).repos.teams user, repo, params
         end
       end
     end
