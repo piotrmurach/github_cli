8cc1d3aebf8ae07a00b8efc2dc5df91e2e6f8cc5
Piotr Murach <pmurach@gmail.com>
2013-04-20 16:59:58 +0100

    Fix github_api spec.

--- spec/github_cli/api_spec.rb
+++ spec/github_cli/api_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe GithubCLI::API do
   context '#github_api' do
-    before(:each) { described_class.send(:class_variable_set, :@@api, nil) }
+    before {
+      described_class.send(:instance_variable_set, :@github_api, nil)
+    }
 
     it 'sets up github api connection' do
       github_instance = stub.as_null_object
       Github.should_receive(:new).and_return github_instance
-      described_class.github_api.should == github_instance
+      expect(described_class.github_api).to eql(github_instance)
     end
 
     it 'does not instantiate on subsequent calls' do
