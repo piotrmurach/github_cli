59e96eb676bf7b595349cd8ea4d2c6bf1e486930
Piotr Murach <pmurach@gmail.com>
2013-04-20 17:37:34 +0100

    Change to take global options.

--- lib/github_cli/commands/assignees.rb
+++ lib/github_cli/commands/assignees.rb
@@ -10,12 +10,18 @@ module GithubCLI
       Lists all the available assignees (owner + collaborators) to which issues may be assigned
     DESC
     def list(user, repo)
-      Assignee.all user, repo, options[:params], options[:format]
+      global_options = options.dup
+      params = options[:params].dup
+      Util.hash_without!(global_options, params.keys + ['params'])
+      Assignee.all user, repo, params, global_options
     end
 
     desc 'check <user> <repo> <assignee>', 'Check if <assignee> is assigned to <repo>'
     def check(user, repo, assignee)
-      Assignee.check user, repo, assignee, options[:params], options[:format]
+      global_options = options.dup
+      params = options[:params].dup
+      Util.hash_without!(global_options, params.keys + ['params'])
+      Assignee.check user, repo, assignee, params, global_options
     end
 
   end # Assignees
