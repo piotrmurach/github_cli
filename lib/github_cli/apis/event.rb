10707173787ef8452eb9582a261499179d714e4b
Piotr Murach <pmurach@gmail.com>
2013-04-26 23:23:49 +0100

    Change event api to take options.

--- lib/github_cli/apis/event.rb
+++ lib/github_cli/apis/event.rb
@@ -5,51 +5,51 @@ module GithubCLI
 
     class << self
 
-      def public(params, format)
-        output format do
-          github_api.activity.events.public params
+      def public(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.public params
         end
       end
 
-      def repository(user, repo, params, format)
-        output format do
-          github_api.activity.events.repository user, repo, params
+      def repository(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.repository user, repo, params
         end
       end
 
-      def issue(user, repo, params, format)
-        output format do
-          github_api.activity.events.issue user, repo, params
+      def issue(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.issue user, repo, params
         end
       end
 
-      def network(user, repo, params, format)
-        output format do
-          github_api.activity.events.network user, repo, params
+      def network(user, repo, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.network user, repo, params
         end
       end
 
-      def organization(org, params, format)
-        output format do
-          github_api.activity.events.organization org, params
+      def organization(org, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.organization org, params
         end
       end
 
-      def received(user, params, format)
-        output format do
-          github_api.activity.events.received user, params
+      def received(user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.received user, params
         end
       end
 
-      def performed(user, params, format)
-        output format do
-          github_api.activity.events.performed user, params
+      def performed(user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.performed user, params
         end
       end
 
-      def user_org(user, org, params, format)
-        output format do
-          github_api.activity.events.user_org user, org, params
+      def user_org(user, org, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).activity.events.user_org user, org, params
         end
       end
     end
