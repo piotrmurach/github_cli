5920737deea821d91b2e756ca10cd14a85c96cb3
Piotr Murach <pmurach@gmail.com>
2013-05-03 23:20:33 +0100

    Change gist api to take options.

--- lib/github_cli/apis/gist.rb
+++ lib/github_cli/apis/gist.rb
@@ -5,63 +5,63 @@ module GithubCLI
 
     class << self
 
-      def all(params, format)
-        output format do
-          github_api.gists.list params
+      def all(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.list params
         end
       end
 
-      def starred(params, format)
-        output format do
-          github_api.gists.starred params
+      def starred(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.starred params
         end
       end
 
-      def get(id, params, format)
-        output format do
-          github_api.gists.get id, params
+      def get(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.get id, params
         end
       end
 
-      def create(params, format)
-        output format do
-          github_api.gists.create params
+      def create(params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.create params
         end
       end
 
-      def edit(id, params, format)
-        output format do
-          github_api.gists.edit id, params
+      def edit(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.edit id, params
         end
       end
 
-      def star(id, params, format)
-        output format do
-          github_api.gists.star id, params
+      def star(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.star id, params
         end
       end
 
-      def unstar(id, params, format)
-        output format do
-          github_api.gists.unstar id, params
+      def unstar(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.unstar id, params
         end
       end
 
-      def starred?(id, params, format)
-        output format do
-          github_api.gists.starred? id, params
+      def starred?(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.starred? id, params
         end
       end
 
-      def fork(id, params, format)
-        output format do
-          github_api.gists.fork id, params
+      def fork(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.fork id, params
         end
       end
 
-      def delete(id, params, format)
-        output format do
-          github_api.gists.delete id, params
+      def delete(id, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).gists.delete id, params
         end
       end
     end
