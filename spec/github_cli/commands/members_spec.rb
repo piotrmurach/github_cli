9e30dfebd9f62ac69ad2bc7f34c9cb68ae79ff26
Piotr Murach <pmurach@gmail.com>
2013-05-13 19:17:49 +0100

    Add specs for member commands.

---
+++ spec/github_cli/commands/members_spec.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe GithubCLI::Commands::Members do
+  let(:format) { {'format' => 'table'} }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github_cli' }
+  let(:org)    { 'github' }
+  let(:api_class) { GithubCLI::Member }
+
+  it "invokes member:list" do
+    api_class.should_receive(:all).with(org, {}, format)
+    subject.invoke "member:list", [org]
+  end
+
+  it "invokes member:list --public" do
+    api_class.should_receive(:all_public).with(org, {}, format)
+    subject.invoke "member:list", [org], {:public => true}
+  end
+
+  it "invokes member:member" do
+    api_class.should_receive(:member?).with(org, user, {}, format)
+    subject.invoke "member:member", [org, user]
+  end
+
+  it "invokes member:member --public" do
+    api_class.should_receive(:member?).with(org, user, {}, format)
+    subject.invoke "member:member", [org, user]
+  end
+
+  it "invokes member:delete" do
+    api_class.should_receive(:delete).with(org, user, {}, format)
+    subject.invoke "member:delete", [org, user]
+  end
+
+  it "invokes member:publicize" do
+    api_class.should_receive(:publicize).with(org, user, {}, format)
+    subject.invoke "member:publicize", [org, user]
+  end
+
+  it "invokes member:conceal" do
+    api_class.should_receive(:conceal).with(org, user, {}, format)
+    subject.invoke "member:conceal", [org, user]
+  end
+end
