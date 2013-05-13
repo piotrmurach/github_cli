1fd8790b9f7075c5ce4792551c7253581e8b8bed
Piotr Murach <pmurach@gmail.com>
2013-05-13 19:18:34 +0100

    Change member api to remove public methods.

--- lib/github_cli/apis/member.rb
+++ lib/github_cli/apis/member.rb
@@ -5,45 +5,33 @@ module GithubCLI
 
     class << self
 
-      def all(org, params, format)
-        output format do
-          github_api.orgs.members.list org, params
+      def all(org, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).orgs.members.list org, params
         end
       end
 
-      def all_public(org, params, format)
-        output format do
-          github_api.orgs.members.list_public org, params
+      def member?(org, user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).orgs.members.member? org, user, params
         end
       end
 
-      def member?(org, user, params, format)
-        output format do
-          github_api.orgs.members.member? org, user, params
+      def delete(org, user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).orgs.members.delete org, user, params
         end
       end
 
-      def public_member?(org, user, params, format)
-        output format do
-          github_api.orgs.members.public_member? org, user, params
+      def publicize(org, user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).orgs.members.publicize org, user, params
         end
       end
 
-      def delete(org, user, params, format)
-        output format do
-          github_api.orgs.members.delete org, user, params
-        end
-      end
-
-      def publicize(org, user, params, format)
-        output format do
-          github_api.orgs.members.publicize org, user, params
-        end
-      end
-
-      def conceal(org, user, params, format)
-        output format do
-          github_api.orgs.members.conceal org, user, params
+      def conceal(org, user, params, options)
+        output options[:format], options[:quiet] do
+          github_api(options).orgs.members.conceal org, user, params
         end
       end
     end
