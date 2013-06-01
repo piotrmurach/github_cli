1e41e6142c68d2a0f6a68aaa1cd298226d5db3fe
Piotr Murach <pmurach@gmail.com>
2013-06-01 20:03:37 +0100

    Change github api test.

---
+++ spec/github_cli/api/github_api_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe GithubCLI::API, '#gihtub_api' do
+  before {
+    described_class.send(:instance_variable_set, :@github_api, nil)
+  }
+
+  it 'sets up github api connection' do
+    github_instance = stub.as_null_object
+    Github.should_receive(:new).and_return github_instance
+    expect(described_class.github_api).to eql(github_instance)
+  end
+
+  it 'does not instantiate on subsequent calls' do
+    described_class.github_api
+    Github.should_receive(:new).exactly(0).times
+    described_class.github_api
+  end
+end # GithubCLI::API
