41a57ccaa532f2d90af3ed1a113bd52baa8d8ea7
Piotr Murach <pmurach@gmail.com>
2013-10-06 16:08:59 +0100

    Add helper for status codes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,32 @@ RSpec.configure do |config|
   config.order = :rand
 end
 
+RSpec::Matchers.define :exit_with_code do |status_code|
+  actual = nil
+
+  match do |block|
+    begin
+      block.call
+    rescue SystemExit => e
+      actual = e.status
+    end
+    actual and actual == status_code
+  end
+
+  failure_message_for_should do |block|
+    "expected block to call exit(#{status_code}) but exit" +
+    (actual.nil? ? " not called" : "(#{actual}) was called")
+  end
+
+  failure_message_for_should_not do |block|
+    "expected block not to call exit(#{status_code})"
+  end
+
+  description do
+    "expect block to call exit(#{status_code})"
+  end
+end
+
 def fixture(name)
   File.read File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures', name)
 end
