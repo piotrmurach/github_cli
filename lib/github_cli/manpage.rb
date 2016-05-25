8f815ed22d52848005540e4c33d41d950034b1f4
Piotr Murach <pmurach@gmail.com>
2016-05-26 00:28:27 +0100

    Change man pages implementation.

--- lib/github_cli/manpage.rb
+++ lib/github_cli/manpage.rb
@@ -6,43 +6,87 @@ module GithubCLI
   module Manpage
     extend self
 
-    MANPAGES = %w[
-      gcli-authorize.1
-      gcli-config.1
-      gcli-repo.1
-      gcli.1
-      gcli-whoami.1
-    ]
+    # Full path to manual direcotry
+    def man_dir
+      File.expand_path('../man', __FILE__)
+    end
 
-    def has?(command)
-      yield self if MANPAGES.include? command
+    # Paths to manpages included in this library
+    #
+    # @api private
+    def manpages(name, section = nil)
+      Dir.entries(man_dir).select do |file|
+        file =~ /(.+)#{name}\.#{section || '\d'}$/
+      end
+    end
+
+    # Check if manual exists for a command
+    #
+    # @param [String] command
+    #   the command name
+    #
+    # @api public
+    def manpage?(name, section = nil)
+      manpages(name, section).any?
     end
 
     # The groff command with extra arguments to turn
     # roff into terminal output
+    #
+    # @api private
     def groff
       "groff -Wall -mtty-char -mandoc -Tascii"
     end
 
     # Check if groff is installed on the system
-    def has_groff?
+    # @api private
+    def groff?
       System.command?('groff')
     end
 
     # Check if man is installed on the system
-    def has_man?
+    def man?
       System.command?('man')
     end
 
-    def show(command)
-      root = File.expand_path("../man", __FILE__)
+    # Read manual page
+    #
+    # @api public
+    def read(name, section = nil)
+      paths = manpages(name)
+      return if paths.empty?
 
-      if has_groff?
-        system "#{groff} #{root}/#{command} | #{Pager.pager_command}"
+      if paths.size == 1
+        manpath = paths[0]
+      elsif paths.size > 1
+        prompt = TTY::Prompt.new
+        manpath = prompt.select("Choose manual to view?", paths)
+      end
+
+      if manpath
+        run(manpath)
       else
-        system "#{Pager.pager_command} #{root}/#{command}.txt"
+        abort("No manuals found for #{name}")
       end
     end
 
+    def available_command
+      if groff?
+        groff
+      elsif man?
+        'man'
+      else
+        nil
+      end
+    end
+
+    def run(manpath)
+      fullpath = File.join(man_dir, manpath)
+      if !(cmd = available_command).nil?
+        system("#{cmd} #{fullpath} | #{Pager.pager_command}")
+      else
+        system("#{Pager.pager_command} #{fullpath}.txt")
+      end
+    end
   end # Manpage
 end # GithubCLI
