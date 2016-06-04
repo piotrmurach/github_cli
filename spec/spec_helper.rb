8c1e6c63158941bdfbe40adf419ec2307e299bea
Piotr Murach <pmurach@gmail.com>
2016-06-04 22:45:21 +0100

    Link fixtures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,30 @@
-require 'rubygems'
-
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'github_cli'
 
+module Helpers
+  def source_root
+    File.join(File.dirname(__FILE__), '..', 'fixtures')
+  end
+
+  def dest_root
+    File.join(File.dirname(__FILE__), '..', 'tmp')
+  end
+
+  def fixture(name)
+    File.read(File.expand_path(File.join(source_root, name)))
+  end
+
+  def fixture_path(name='')
+    File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures', name)
+  end
+end
+
 RSpec.configure do |config|
   config.order = :rand
+  config.include(Helpers)
 end
 
 RSpec::Matchers.define :exit_with_code do |status_code|
@@ -36,10 +53,5 @@ RSpec::Matchers.define :exit_with_code do |status_code|
   end
 end
 
-def fixture(name)
-  File.read File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures', name)
-end
 
-def fixture_path(name='')
-  File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures', name)
-end
+
