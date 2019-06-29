de545bd00cd6377ebe9dc6c21c0c143898697641
Piotr Murach <pmurach@gmail.com>
2019-06-29 13:45:58 +0200

    Change apis loading

--- lib/github_cli/apis.rb
+++ lib/github_cli/apis.rb
@@ -1,14 +1,4 @@
-if defined?(require_relative)
-  def require_api(path)
-    require_relative "apis/#{path}"
-  end
-else
-  def require_api(path)
-    require "github_cli/apis/#{path}"
-  end
-end
-
 Dir[File.dirname(__FILE__) + '/apis/*.rb'].sort.each do |path|
   filename = File.basename(path)
-  require_api(filename)
+  require_relative("apis/#{filename}")
 end
