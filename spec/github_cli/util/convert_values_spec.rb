d199326c34e158bd7f1babb82c985a4d56525980
Piotr Murach <pmurach@gmail.com>
2019-07-06 13:31:02 +0200

    Change to remove direct helper loading

--- spec/github_cli/util/convert_values_spec.rb
+++ spec/github_cli/util/convert_values_spec.rb
@@ -1,8 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-require 'spec_helper'
-
-describe GithubCLI::Util, '#convert_values' do
+RSpec.describe GithubCLI::Util, '#convert_values' do
   let(:values) { [true, {:a => false }, 'string']}
 
   it { expect(subject.convert_values(values)).to include(["false"]) }
