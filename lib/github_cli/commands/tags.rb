164b3e7e71ca493bd01905179ed0058d31dcb076
Piotr Murach <pmurach@gmail.com>
2019-07-16 22:18:48 +0200

    Change to update tag command in ref issue #24

--- lib/github_cli/commands/tags.rb
+++ lib/github_cli/commands/tags.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../apis/tag'
-require_relative '../util'
-require_relative '../command'
+require_relative "../apis/tag"
+require_relative "../util"
+require_relative "../command"
 
 module GithubCLI
   module Commands
@@ -10,23 +10,23 @@ module GithubCLI
 
       namespace :tag
 
-      desc 'get <user> <repo> <sha>', 'Get a Tag'
+      desc "get <user> <repo> <sha>", "Get a Tag"
       method_option :recursive, :type => :boolean, :aliases => ["-r"],
-                    :desc => 'get a tree recursively'
+                    :desc => "get a tree recursively"
       def get(user, repo, sha)
         global_options = options.dup
         params = options[:params].dup
-        Util.hash_without!(global_options, params.keys + ['params'])
+        Util.hash_without!(global_options, params.keys + ["params"])
         Tag.get user, repo, sha, params, global_options
       end
 
-      desc 'create <user> <repo>', 'Create a Tag Object'
-      option :tag, :type => :string, :desc => "String of the tag"
-      option :message, :type => :string, :aliases => ["-m"],
+      desc "create <user> <repo>", "Create a Tag Object"
+      option :tag, :type => :string, :desc => "String of the tag", :required => true
+      option :message, :type => :string, :aliases => ["-m"], :required => true,
         :desc => "String of the tag message"
-      option :object, :type => :string, :banner => "<SHA>",
+      option :object, :type => :string, :banner => "<SHA>", :required => true,
         :desc => "String of the SHA of the git object this is tagging"
-      option :type, :type => :string, :banner => "commit/tree/blob",
+      option :type, :type => :string, :banner => "commit/tree/blob", :required => true,
         :desc => "String of the type of the object we're tagging."
       option :tagger, :type => :hash, :desc => "Tag author information"
       long_desc <<-DESC
@@ -38,23 +38,27 @@ module GithubCLI
 
         Parameters
 
-        tag - String of the tag \n
-        message - String of the tag message \n
-        object - String of the SHA of the git object this is tagging \n
-        type - String of the type of the object we're tagging. Normally this is a commit but it can also be a tree or a blob \n
-        tagger.name - String of the name of the author of the tag \n
-        tagger.email - String of the email of the author of the tag \n
-        tagger.date - Timestamp of when this object was tagged \n
+        tag - string - Required. The tag's name. This is typically a version (e.g., "v0.0.1").\n
+        message string  Required. The tag message.\n
+        object  string  Required. The SHA of the git object this is tagging.\n
+        type  string  Required. The type of the object we're tagging. Normally this is a commit but it can also be a tree or a blob.\n
+        tagger - hash - An object with information about the individual creating the tag.\n
+
+        The tagger hash contains the following keys:
+
+        name - string - The name of the author of the tag\n
+        email - string - The email of the author of the tag\n
+        date - string - When this object was tagged. This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
       DESC
       def create(user, repo)
         global_options = options.dup
         params = options[:params].dup
-        params['tag']     = options[:tag] if options[:tag]
-        params['message'] = options[:message] if options[:message]
-        params['object']  = options[:object] if options[:object]
-        params['type']    = optiosn[:type] if options[:type]
-        params['tagger']  = options[:tagger] if options[:tagger]
-        Util.hash_without!(global_options, params.keys + ['params'])
+        params["tag"]     = options[:tag]
+        params["message"] = options[:message]
+        params["object"]  = options[:object]
+        params["type"]    = options[:type]
+        params["tagger"]  = options[:tagger] if options.key?("tagger")
+        Util.hash_without!(global_options, params.keys + ["params"])
         Tag.create user, repo, params, global_options
       end
     end # Tag
--- spec/github_cli/commands/tags_spec.rb
+++ spec/github_cli/commands/tags_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe GithubCLI::Commands::Tags do
-  let(:format) { {'format' => 'table'} }
-  let(:user)   { 'peter-murach' }
-  let(:repo)   { 'github_cli' }
-  let(:sha)    { '3a0f86fb8db8eea7ccbb9a95f325ddbedfb25e15' }
+  let(:format) { {"format" => "table"} }
+  let(:user)   { "piotrmurach" }
+  let(:repo)   { "github_cli" }
+  let(:sha)    { "3a0f86fb8db8eea7ccbb9a95f325ddbedfb25e15" }
   let(:api_class) { GithubCLI::Tag }
 
   it "invokes tag:get" do
@@ -13,12 +13,18 @@ RSpec.describe GithubCLI::Commands::Tags do
   end
 
   it "invokes tag:create" do
-    expect(api_class).to receive(:create).with(user, repo, {}, format)
-    subject.invoke "tag:create", [user, repo]
-  end
-
-  it "invokes tag:create --object" do
-    expect(api_class).to receive(:create).with(user, repo, {'object' => sha}, format)
-    subject.invoke "tag:create", [user, repo], :object => sha
+    opts = {
+      "tag" => "v0.0.1",
+      "message" => "initial version\n",
+      "object" => "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
+      "type" => "commit",
+      "tagger" => {
+          "name" => "Monalisa Octocat",
+          "email" => "octocat@github.com",
+          "date" => "2011-06-17T14:53:35-07:00"
+      }
+    }
+    expect(api_class).to receive(:create).with(user, repo, opts, format)
+    subject.invoke "tag:create", [user, repo], opts
   end
 end
